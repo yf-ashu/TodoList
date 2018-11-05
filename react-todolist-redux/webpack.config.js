34035cac71ac0bbbf560f9bcfd4c07d2d758f298
yf-ashu <yfcai1227@gmail.com>
2018-11-05 10:45:31 +0800

    src change

--- react-todolist-redux/webpack.config.js
+++ react-todolist-redux/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
-    publicPath: '/'
+    publicPath: './'
   },
   devServer: {
     historyApiFallback: true,
